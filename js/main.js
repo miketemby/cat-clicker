c5b5aad381ce6b66e0eaf0eec4571b409ded2d47
miketemby <mike@digitalserviceslab.com.au>
2018-11-24 15:49:37 +1100

    refactor: Update styles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const img = document.getElementById('cat-image');
 const clickCounter = (function() {
 let clicks = 0;
 let countContainer = document.getElementById('click-count')
+countContainer.textContent = '0';
 
     return function clickCounter(evt) {
         clicks +=1;
