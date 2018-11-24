f02c3c6eedc3a0d0231b7ea10e2fb604223d6f37
miketemby <mike@digitalserviceslab.com.au>
2018-11-24 15:34:50 +1100

    refactor: Updates to js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 // get image element
-const img = document.getElementById('cat-img');
+const img = document.getElementById('cat-image');
+
+
+const clickCounter = (function() {
+let clicks = 0;
+let countContainer = document.getElementById('click-count')
+
+    return function clickCounter(evt) {
+        clicks +=1;
+        countContainer.textContent = `${clicks}`;
+
+    };
+
+})();
 
 //add event listner
 img.addEventListener('click', clickCounter);
-
-(function() {
-let clicks = 0;
-let countContainer = document.getElementById('click-count')
-    return function clickCounter(evt) {
-        clicks +=1;
-        clickCounter.textContent = `${clicks}`;
-
-    };
-    
-})();
