194d62849efee415fa29a8f3323842deea76400c
miketemby <mike@digitalserviceslab.com.au>
2018-11-25 10:50:47 +1100

    refactor: Manu functional updates
    Added a list of cats which can be clicked and have their own count

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// get image element
-const img = document.getElementById('cat-image');
-
 // create cat class
 class Cat{
     constructor(name, image) {
@@ -8,6 +5,7 @@ class Cat{
     this.name = name;
     this.image = image;
     this.clicks = 0;
+    this.html = this.constructHTML();
                   
     }
 
@@ -16,23 +14,100 @@ class Cat{
         return this.clicks;
     };
 
+
+    constructHTML() {
+        let catContainer = document.createElement('section');
+        let clickCounter = document.createElement('div');
+        let image = document.createElement('img');
+        let nameBox = document.createElement('div')
+
+        clickCounter.textContent = this.clicks;
+        clickCounter.classList.add('click-container');
+    
+        image.setAttribute('src',this.image);
+        image.classList.add('cat-image');
+    
+        nameBox.textContent = this.name;
+        nameBox.classList.add('name-container');
+
+        image.addEventListener('click', (function(thisCat){
+
+            return function(evt) {
+                thisCat.updateClicks();
+                evt.target.previousSibling.textContent = thisCat.clicks;
+            };
+
+        })(this));
+
+        catContainer.appendChild(clickCounter);
+        catContainer.appendChild(image);
+        catContainer.appendChild(nameBox);
+        catContainer.classList.add('cat-container');
+
+        return catContainer;
+
+    }
+
 };
 
+// add all cats to an array
+let cats = [new Cat('Harry', 'img/cat.jpg'), new Cat('Larry', 'img/cat-2.jpg'), new Cat('Barry', 'img/cat-3.jpg'), new Cat('Sally', 'img/cat-4.jpg'), new Cat('Philbert', 'img/cat-5.jpg'),];
+const body = document.getElementById('body');
+const foot = document.getElementById('footer');
 
-const clickCounter = (function() {
-let clicks = 0;
-let countContainer = document.getElementById('click-count');
-let cats = [new Cat('Harry', 'img/cat.jpg'), new Cat('Larry', 'img/cat-2.jpg')];
+// create html
+for(cat of cats) {
 
-countContainer.textContent = '0';
+    // body.insertBefore(cat.html,foot);
 
-    return function clickCounter(evt) {
-        clicks +=1;
-        countContainer.textContent = `${clicks}`;
+}
 
-    };
+// TO DO
+// create side by side columns for cat area and cat list
+const catArena = document.createElement('section');
+catArena.setAttribute('id', 'cat-arena');
 
-})();
+const catListContainer = document.createElement('section');
+catListContainer.setAttribute('id', 'cat-list-container');
 
-//add event listner
-img.addEventListener('click', clickCounter);
+body.insertBefore(catArena, foot);
+body.insertBefore(catListContainer, foot);
+
+
+// TO DO
+// create an unordered list of cats
+
+let catsList = document.createElement('ul');
+catsList.setAttribute('id', 'cat-list');
+for(cat of cats) {
+    let catItem = document.createElement('li');
+    catItem.classList.add('cat-list-item');
+
+    let img = document.createElement('img');
+    img.setAttribute('src', cat.image);
+
+    catItem.innerHTML = img.outerHTML;
+
+    catItem.addEventListener('click', (function(thisCat){
+
+        return function() {
+            changeCats(thisCat);
+        };
+
+    })(cat));
+
+
+
+    catsList.appendChild(catItem);
+}
+
+catListContainer.appendChild(catsList);
+
+
+// TO DO
+// build logic to remove current cat and add new cat when selected from list
+
+function changeCats(cat) {
+    catArena.innerHTML = '';
+    catArena.appendChild(cat.html);
+}
