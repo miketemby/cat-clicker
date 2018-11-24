d986c0563e1ae64575699ae4097077b901888608
miketemby <mike@digitalserviceslab.com.au>
2018-11-24 16:24:49 +1100

    refactor: Add cat class

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 // get image element
 const img = document.getElementById('cat-image');
 
+// create cat class
+class Cat{
+    constructor(name, image) {
+
+    this.name = name;
+    this.image = image;
+    this.clicks = 0;
+                  
+    }
+
+    updateClicks() {
+        this.clicks ++;
+        return this.clicks;
+    };
+
+};
+
 
 const clickCounter = (function() {
 let clicks = 0;
-let countContainer = document.getElementById('click-count')
+let countContainer = document.getElementById('click-count');
+let cats = [new Cat('Harry', 'img/cat.jpg'), new Cat('Larry', 'img/cat-2.jpg')];
+
 countContainer.textContent = '0';
 
     return function clickCounter(evt) {
